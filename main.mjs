e0a138429793bdc16aee18b9c21291ab7e4fdb25
moonrailgun <moonrailgun@gmail.com>
2023-10-26 00:04:51 +0800

    update test action and format code

--- main.mjs
+++ main.mjs
@@ -6,17 +6,17 @@ const input = {
   serverUrl: core.getInput('server-url'),
   lafPat: core.getInput('laf-pat'),
   distPath: core.getInput('dist-path'),
-  bucketName:core.getInput('bucket-name'),
+  bucketName: core.getInput('bucket-name'),
 };
 
-$`npm i laf-cli -g`
+$`npm i laf-cli -g`;
 
-$`laf user add dev -r ${input.serverUrl}`
-$`laf user switch dev`
-$`laf user list`
-$`laf login ${input.lafPat}`
-$`laf storage list`
+$`laf user add dev -r ${input.serverUrl}`;
+$`laf user switch dev`;
+$`laf user list`;
+$`laf login ${input.lafPat}`;
+$`laf storage list`;
 
-cd(input.distPath)
+cd(input.distPath);
 
-$`laf storage push ${input.bucketName} ./`
+$`laf storage push ${input.bucketName} ./`;
